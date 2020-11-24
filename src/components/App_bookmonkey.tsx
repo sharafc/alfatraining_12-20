372606da365ffc6360330c9d8b4c529ab2dd0ad3
Christian Sharaf <sharafc@users.noreply.github.com>
2020-11-24 14:22:31 +0100

    Add skeleton for bookmonkey

---
+++ src/components/App_bookmonkey.tsx
@@ -0,0 +1,37 @@
+import React, { ReactElement, useState } from "react";
+import BookList from "./BookList";
+import BookDetails from "./BookDetails";
+import Book from "../types/Book";
+
+export default function App(): ReactElement {
+    const [viewState, setViewState] = useState<string>("list");
+    const [book, setBook] = useState<Book>();
+
+    const clickedBook = (currentBook: Book): void => {
+        setBook(book ? undefined : currentBook);
+        setViewState("detail");
+    };  
+
+    const showList = (): void => {
+        setBook(undefined);
+        setViewState("list");
+    }
+
+    const selectBookView = () => {
+        if (viewState === "list") {
+            return <BookList clickedBook={clickedBook} />;
+        }
+        if (viewState === "detail" && book) {
+            return <BookDetails selectedBook={book} showList={showList} />;
+        }
+    };
+
+    return (
+        <div className="ui container">
+            {/**
+             * defer logic of choosing correct template to its own function
+             */}
+            {selectBookView()}
+        </div>
+    );
+}
